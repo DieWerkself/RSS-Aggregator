b4bfd2423a44c8cff03d3089e52a85261ecdc24e
diewerkself <redead29@gmail.com>
2023-06-03 19:16:04 +0300

    Adding validation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,48 @@
 import './styles.scss';
+import render from './view.js';
 import 'bootstrap';
+import * as yup from 'yup';
+import onChange from 'on-change';
 
-console.log("Test!!");
+const elements = {
+  form: document.querySelector('form'),
+  input: document.querySelector('#url-input'),
+  feedback: document.querySelector('.feedback'),
+};
+
+const initialState = {
+  status: {
+    validation: null,
+    network: null,
+  },
+  feeds: [],
+};
+
+const state = onChange(initialState, render(elements, initialState));
+
+const loadedFeeds = () => initialState.feeds.map((item) => item.feedUrl);
+
+elements.form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  const url = data.get('url');
+  const existingUrls = loadedFeeds();
+  const urlValidate = yup.string().url('Ссылка должна быть валидным URL').notOneOf(existingUrls, 'RSS уже существует');
+  urlValidate.validate(url).then((correctUrl) => {
+    state.feeds.push({ feedUrl: correctUrl });
+    state.status = { validation: null, network: 'RSS успешно загружен' };
+  }).catch((error) => {
+    state.status = { validation: error.message, network: null } ;
+  });
+})
+
+// https://news.yandex.ru/daily.rss (пустышка, не содержит rss - "Ресурс не содержит валидный RSS")
+// Отечественные и зарубежные rss - каналы:
+// https://aljazeera.com/xml/rss/all.xml
+// https://buzzfeed.com/world.xml
+// https://thecipherbrief.com/feed
+// https://feeds.washingtonpost.com/rss/world (отвечает долго, в районе 4-5 секунд, иногда и до 10 доходит)
+// https://rt.com/rss/news
+// http://www.dp.ru/exportnews.xml
+// http://www.fontanka.ru/fontanka.rss
+// http://lenta.ru/l/r/EX/import.rss
---
+++ src/view.js
@@ -0,0 +1,36 @@
+const renderStatus = (elements, status) => {
+  if (!status.validation && status.network) {
+    elements.feedback.classList.remove('text-danger');
+    elements.feedback.classList.add('text-success');
+    elements.feedback.textContent = status.network;
+    elements.form.reset();
+    elements.input.focus();
+  }
+  if (status.validation && !status.network) {
+    elements.feedback.classList.remove('text-success');
+    elements.feedback.classList.add('text-danger');
+    elements.feedback.textContent = status.validation;
+  }
+
+};
+
+const renderFeeds = (elements, error) => {
+  return
+};
+
+
+
+export default (elements, initialState) => (path, value, prevValue) => {
+  console.log(path, value);
+  switch (path) {
+    case 'feeds':
+      renderFeeds(elements, value);
+      break
+    case 'status':
+      renderStatus(elements, value, prevValue);
+      break;
+    default:
+      break;
+  }
+};
+
